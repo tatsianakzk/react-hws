27a51d121f10ee2340ec3e422c3d6dde52cc55df
Tatsiana <kazak.tatsiana@student.ehu.lt>
2024-12-02 20:07:36 +0100

    hw-05-add-solution

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,12 +3,14 @@ import './App.css';
 import Header from './Components/page_component/Header/Header';
 import Main from './Components/page_component/Main/Main';
 import Footer from './Components/page_component/Footer/Footer';
+import Home from './Components/Pages/Home/Home'
+import Menu from './Components/Pages/Menu/Menu'
 
 const App = () => {
   return (
     <>
       <Header />
-      <Main />
+      <Home />
       <Footer />
     </>
   );
--- vite-project/src/Components/page_component/Main/Main.jsx
+++ vite-project/src/Components/page_component/Main/Main.jsx
@@ -1,36 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Main.css';
-import Button from '../../reusable_components/Button/Button';
-import CardList from '../../reusable_components/Card/Card';
+import Menu from '../../Pages/Menu/Menu'
+import Home from '../../Pages/Home/Home'
 
 const Main = () => {
-    const [selectedCategory, setSelectedCategory] = useState('');
 
-    const handleFilterClick = (category) => {
-        setSelectedCategory(category);
-    };
+    <Home />
 
-    return (
-        <main className="main-section">
-            <h1 className='MenuTitle'>
-                Browse our menu
-            </h1>
-            <h3 className='MenuSubtitle'>
-                Use our menu to place an order online, or <span className='tooltip'>phone <span className="tooltip-text">+1 (234) 567-890</span></span> our store to place a pickup order. Fast and fresh food.
-            </h3>
-            <div className='ButtonSection'>
-                <Button text="Dessert" onClick={() => handleFilterClick('Dessert')} isActive={selectedCategory === 'Dessert'} />
-                <Button text="Dinner" onClick={() => handleFilterClick('Dinner')} isActive={selectedCategory === 'Dinner'} />
-                <Button text="Breakfast" onClick={() => handleFilterClick('Breakfast')} isActive={selectedCategory === 'Breakfast'} />
-            </div>
-
-            <div className='CardListSection'>
-                <CardList category={selectedCategory} />
-            </div>
-
-            <Button text="See more" isActive customClass='more-button' />
-        </main>
-    );
 };
 
 export default Main;
